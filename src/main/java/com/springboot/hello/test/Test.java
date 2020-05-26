93d4b08842e810ea2b4aeb422eb0151982cba816
1134005157 <1134005157@qq.com>
2020-05-26 19:45:54 +0800

    read file

--- src/main/java/com/springboot/hello/test/Test.java
+++ src/main/java/com/springboot/hello/test/Test.java
@@ -7,11 +7,23 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) throws ParseException {
-        String url = "https://beta-microsite-cg.myscrm.cn/api/index.php?r=oauth2/token&org_code=yjlkhadmin_cg&reg_from=99";
-        if (url.contains("&org_code=") || url.contains("&req_from=")) {
-            System.out.println(" contains");
-        } else {
-            System.out.println(" not");
+//        String url = "https://beta-microsite-cg.myscrm.cn/api/index.php?r=oauth2/token&org_code=yjlkhadmin_cg&reg_from=99";
+//        if (url.contains("&org_code=") || url.contains("&req_from=")) {
+//            System.out.println(" contains");
+//        } else {
+//            System.out.println(" not");
+//        }
+        int type = 2;
+        int status = 1;
+        switch (status) {
+            case 1:
+                System.out.println(" status == 1");
+                if (1 == type) {
+                    System.out.println(" type == 1");
+                }
+                break;
+            default:
+                break;
         }
     }
 }
--- src/test/java/com/springboot/hello/HelloApplicationTests.java
+++ src/test/java/com/springboot/hello/HelloApplicationTests.java
@@ -1,13 +1,13 @@
 package com.springboot.hello;
 
 
-import org.junit.Test;
+//import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 		System.out.println(" --- test001 --- ");
 	}
