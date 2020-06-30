67a09e9413e4fd031142afbdd58ca625f712dd88
1134005157 <1134005157@qq.com>
2020-06-30 16:57:22 +0800

    reflection

--- src/main/java/com/springboot/hello/HelloApplication.java
+++ src/main/java/com/springboot/hello/HelloApplication.java
@@ -18,13 +18,14 @@ import java.util.Arrays;
 public class HelloApplication {
 
 	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication();
+		application.setAddCommandLineProperties(false);
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
