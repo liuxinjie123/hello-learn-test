b8c9eb074fdf63f85613078e5f327fca82a4b2d3
1134005157 <1134005157@qq.com>
2020-07-02 15:44:39 +0800

    reflection

---
+++ src/main/java/com/springboot/hello/config/JsonConfiguration.java
@@ -0,0 +1,18 @@
+package com.springboot.hello.config;
+
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+
+//@JsonComponent
+public class JsonConfiguration {
+
+//    public static class Serializer extends JsonSerializer<> {
+//
+//    }
+//
+//    public static class Deserializer extends JsonDeserializer<> {
+//
+//    }
+
+}
--- src/main/java/com/springboot/hello/config/MyConfiguration.java
+++ src/main/java/com/springboot/hello/config/MyConfiguration.java
@@ -1,11 +1,23 @@
 package com.springboot.hello.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.springboot.hello.property.AcmeProperties;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.JsonbHttpMessageConverter;
 
 @Configuration(proxyBeanMethods = false)
-@EnableConfigurationProperties(value = {AcmeProperties.class})
+//@EnableConfigurationProperties(value = {AcmeProperties.class})
 public class MyConfiguration {
+
+//    @Bean
+//    public HttpMessageConverters customerConverter() {
+//        HttpMessageConverter<?> additional = new FastJsonHttpMessageConverter();
+//        HttpMessageConverter<?> another = new JsonbHttpMessageConverter();
+//        return new HttpMessageConverters(additional, another);
+//    }
 
 }
