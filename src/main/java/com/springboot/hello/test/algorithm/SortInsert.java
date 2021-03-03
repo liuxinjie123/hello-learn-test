7082581521e92e57724488dd8792b46026f3f2d6
1134005157 <1134005157@qq.com>
2021-03-03 16:30:51 +0800

    algorithm

---
+++ src/main/java/com/springboot/hello/test/algorithm/SortInsert.java
@@ -0,0 +1,46 @@
+package com.springboot.hello.test.algorithm;
+
+/**
+ * 插入排序，一般也被称为直接插入排序。
+ *
+ * 对于少量元素的排序，它是一个有效的算法。
+ * 插入排序是一种最简单的排序方法，它的基本思想是将一个记录插入到已经排好序的有序表中，
+ * 从而一个新的、记录数增1的有序表。
+ *
+ * 在其实现过程使用双层循环，外层循环对除了第一个元素之外的所有元素，内层循环对当前元素前面有序表进行待插入位置查找，
+ * 并进行移动。
+ */
+public class SortInsert {
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{3,1,6,7,9,122344,4656,39,33,4656,5,126,77,8,9,343,57765,23,12321};
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
+        System.out.println("\n");
+
+        sort(arr);
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
+        System.out.println("\n");
+    }
+
+    static void sort(int[] arr) {
+        for (int i=1; i<arr.length; i++) {
+            for (int j=i; j<arr.length; j++) {
+                if (arr[j] < arr[j-1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+            for (int k=0; k<arr.length; k++) {
+                System.out.print(" " + arr[k]);
+            }
+            System.out.println("\n");
+        }
+    }
+
+}
