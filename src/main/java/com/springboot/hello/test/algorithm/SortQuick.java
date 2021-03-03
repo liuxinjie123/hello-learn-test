da9823714b093fd3cbc49547bb6476050ecc37f6
1134005157 <1134005157@qq.com>
2021-03-03 16:10:53 +0800

    algorithm

--- src/main/java/com/springboot/hello/test/algorithm/SortQuick.java
+++ src/main/java/com/springboot/hello/test/algorithm/SortQuick.java
@@ -1,18 +1,51 @@
 package com.springboot.hello.test.algorithm;
 
+/**
+ * 它的基本思想是：
+ *
+ * 通过一趟排序将要排序的数据分割成独立的两部分，其中一部分的所有数据都比另外一部分的所有数据都要小，然后再按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此达到整个数据变成有序序列。
+ */
 public class SortQuick {
     public static void main(String[] args) {
-        int arr[] = new int[]{3,3,3,7,9,122344,4656,34,34,4656,5,6,7,8,9,343,57765,23,12321};
-        int len = arr.length-1;
-        arr = qsort(arr,0,len);
-        for (int i:arr) {
-            System.out.print(i+"\t");
+        int arr[] = new int[]{3,1,6,7,9,122344,4656,39,33,4656,5,126,77,8,9,343,57765,23,12321};
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(" " + arr[i]);
         }
+        System.out.println("\n");
+
+        qsort(arr, 0, arr.length-1);
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
+        System.out.println("\n");
     }
 
-    static int[] qsort(int[] arr, int start, int end) {
-
-        return arr;
+    static void qsort(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        int i = start;
+        int j = end;
+        while (i < j) {
+            while ((i < j) && arr[i] < pivot) {
+                i++;
+            }
+            while ((i < j) && arr[j] > pivot) {
+                j--;
+            }
+            if (arr[i] == arr[j] && i < j) {
+                i++;
+            } else {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            if (i-1 > start) {
+                qsort(arr, 0, i-1);
+            }
+            if (j+1 < end) {
+                qsort(arr, j+1, end);
+            }
+        }
     }
 
 }
--- src/main/java/com/springboot/hello/test/algorithm/SortSelect.java
+++ src/main/java/com/springboot/hello/test/algorithm/SortSelect.java
@@ -1,8 +1,18 @@
 package com.springboot.hello.test.algorithm;
 
+/**
+ * 选择排序（Selection sort）是一种简单直观的排序算法。
+ *
+ * 它的工作原理是：
+ *
+ * 第一次从待排序的数据元素中选出最小（或最大）的一个元素，存放在序列的起始位置，
+ * 然后再从剩余的未排序元素中寻找到最小（大）元素，然后放到已排序的序列的末尾。
+ *
+ * 以此类推，直到全部待排序的数据元素的个数为零。选择排序是不稳定的排序方法。
+ */
 public class SortSelect {
     public static void main(String[] args) {
-        int arr[] = new int[]{3,3,3,7,9,122344,4656,34,34,4656,5,6,7,8,9,343,57765,23,12321};
+        int arr[] = new int[]{3,1,6,7,9,122344,4656,39,33,4656,5,126,77,8,9,343,57765,23,12321};
         for (int i=0; i<arr.length; i++) {
             System.out.print(" " + arr[i]);
         }
